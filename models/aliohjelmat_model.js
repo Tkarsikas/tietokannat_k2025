2f1ed96b049e6128ced2dbcfaf8389227f6ac832
Tommi <tommi.karsi@gmail.com>
2025-07-25 21:20:55 +0300

    aliohjelma muutos

--- models/aliohjelmat_model.js
+++ models/aliohjelmat_model.js
@@ -4,8 +4,8 @@ const aliohjelmat = {
     opiskelija_keskiarvo: function(id, callback) {
         return db.query('call opiskelija_keskiarvo(?)', [id], callback);
     },
-    kurssi_keskiarvo: function(id, callback) {
-        return db.query('call kurssi_keskiarvo(?)', [id], callback);
+    opintojakso_keskiarvo: function(id, callback) {
+        return db.query('call opintojakso_keskiarvo(?)', [id], callback);
     }
 };
 
